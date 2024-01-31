b486bd5ed5fc01be2f1142f2e184f0b55430ce38
ambrose <kolus7381@gmail.com>
2024-02-01 00:02:58 +0100

    try

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,26 +5,14 @@ Create a class BasicCache that inherits from
 BaseCaching and is a caching system:
 """
 
-
-from requests_cache import BaseCache
-
-
-BaseCache = __import__('baseclass').BaseCaching
+BaseCaching = __import__('baseclass').BaseCaching
 
 
-class BasicCache(BaseCache):
+class BasicCache(BaseCaching):
     """
     You must use self.cache_data - dictionary from the
     parent class BaseCaching
     This caching system doesn't have limit
-    def put(self, key, item):
-    Must assign to the dictionary self.cache_data the item
-    value for the key key.
-        If key or item is None, this method should not do anything.
-    def get(self, key):
-        Must return the value in self.cache_data linked to key.
-        If key is None or if the key doesn't exist in self.cache_data,
-        return None.
     """
 
     def put(self, key, item):
